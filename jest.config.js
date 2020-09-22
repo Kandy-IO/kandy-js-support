50e21ad5c7925fd2b67c1a04754500a88dba0eb7
Jean-Yves Boudreau <jy.boudreau@gmail.com>
2020-09-21 21:44:41 -0400

    Added example test

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['**/src/**/*.js', '!**/node_modules/**'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  projects: ['<rootDir>/packages/sdp-handlers']
+}
---
+++ packages/sdp-handlers/src/index.js
@@ -0,0 +1,5 @@
+function exampleHandler () {
+  return 'sdp'
+}
+
+module.exports = { exampleHandler }
