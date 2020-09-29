647caf40ee8c988abd73793d325e15c02a28bbf9
Rob Woods <Rob.Woods@kandy.io>
2020-09-29 15:46:49 -0400

    Updating emea and uae configs with real servers

--- packages/link-config-emea/index.js
+++ packages/link-config-emea/index.js
@@ -5,12 +5,12 @@
 const config = {
   authentication: {
     server: {
-      base: 'emea server'
+      base: 'spidr-em.genband.com'
     }
   },
   subscription: {
     websocket: {
-      server: 'emea server'
+      server: 'spidr-em.genband.com'
     }
   },
   call: {
@@ -19,11 +19,19 @@ const config = {
     serverProvidedTurnCredentials: true,
     iceserver: [
       {
-        url: 'emea turn server',
+        url: 'turns:turn-em-1.genband.com:443?transport=tcp',
         credentials: ''
       },
       {
-        url: 'emea stun server',
+        url: 'turns:turn-em-2.genband.com:443?transport=tcp',
+        credentials: ''
+      },
+      {
+        url: 'stun:turn-em-1.genband.com:3478?transport=udp',
+        credentials: ''
+      }
+      {
+        url: 'stun:turn-em-2.genband.com:3478?transport=udp',
         credentials: ''
       }
     ]
--- packages/link-config-uae/index.js
+++ packages/link-config-uae/index.js
@@ -5,12 +5,12 @@
 const config = {
   authentication: {
     server: {
-      base: 'uae server'
+      base: 'ct-webrtc.etisalat.ae'
     }
   },
   subscription: {
     websocket: {
-      server: 'uae server'
+      server: 'ct-webrtc.etisalat.ae'
     }
   },
   call: {
@@ -19,11 +19,19 @@ const config = {
     serverProvidedTurnCredentials: true,
     iceserver: [
       {
-        url: 'uae turn server',
+        url: 'turns:ct-turn1.etisalat.ae:443?transport=tcp',
         credentials: ''
       },
       {
-        url: 'uae stun server',
+        url: 'turns:ct-turn2.etisalat.ae:443?transport=tcp',
+        credentials: ''
+      },
+      {
+        url: 'stun:ct-turn1.etisalat.ae:3478?transport=udp',
+        credentials: ''
+      }
+      {
+        url: 'stun:ct-turn2.etisalat.ae:3478?transport=udp',
         credentials: ''
       }
     ]
